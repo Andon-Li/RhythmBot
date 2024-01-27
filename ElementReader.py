74fb96567b686b33e1455b556b365bcb0b7a1576
Andon Li <andonli281@gmail.com>
2024-01-27 00:30:38 -0500

    New Commit

--- ElementReader.py
+++ ElementReader.py
@@ -1,20 +1,38 @@
 from multiprocessing import Process, Queue
 from time import time, sleep
 import win32gui
-import pygetwindow
+import pygetwindow as pgw
+import mss.tools
 
-# game_element format: (lane_number(0-4), element_type(0-1), timestamp)
-# Element types:
-# Down note
-# Release
+import cv2 as cv
+import numpy as np
+
+
+def grab_image_by_decimal(sct, monitor_number, top_dec, left_dec, width_dec=0.0, height_dec=0.0):
+    monitor = sct.monitors[monitor_number]
+    top_px = int(monitor["height"]*top_dec + monitor["top"])
+    left_px = int(monitor["width"]*left_dec + monitor["left"])
+    if width_dec:
+        width_px = int(monitor["width"] * width_dec)
+    else:
+        width_px = 1
+    if height_dec:
+        height_px = int(monitor["height"] * height_dec)
+    else:
+        height_px = 1
+    return sct.grab({"top": top_px,
+                     "left": left_px,
+                     "width": width_px,
+                     "height": height_px,
+                     "mon": monitor_number})
 
 
 def read_elements(element_queue):
-    window = pygetwindow.getWindowsWithTitle("Fortnite")[0]
+    with mss.mss() as sct:
+        sct_img = grab_image_by_decimal(sct, 2, 0.46, 0.4, 0.205, 0.0)
 
-    print(window)
-    print(window.topleft)
-    print(window.bottomright)
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output="output.png")
+        print("-------------------")
 
 
-
+read_elements(None)
--- InputManager.py
+++ InputManager.py
@@ -5,9 +5,5 @@ import pydirectinput as pdi
 
 # game_element format: (lane number(0-4), element type(0-2), UTC for input)
 def send_inputs(element_queue: Queue, bindings):
-    counter = 0
     while True:
-        lane, el_type, time1 = element_queue.get(block=True)
-        counter += 1
-        print(counter)
-
+        pass
