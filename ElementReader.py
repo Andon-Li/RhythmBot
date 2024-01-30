d051a11e12a77dea9f4a2e8b843d2369dc4bda1f
Andon-Li <60678140+Andon-Li@users.noreply.github.com>
2024-01-29 20:00:58 -0500

    New Commit

--- ElementReader.py
+++ ElementReader.py
@@ -10,7 +10,7 @@ import cv2 as cv
 import numpy as np
 
 
-def grab_image_by_decimal(sct, monitor_number, top_dec, left_dec, width_dec=0.0, height_dec=0.0):
+def dec_to_px(sct, monitor_number, top_dec, left_dec, width_dec=0.0, height_dec=0.0):
     monitor = sct.monitors[monitor_number]
     top_px = int(monitor["height"]*top_dec + monitor["top"])
     left_px = int(monitor["width"]*left_dec + monitor["left"])
@@ -22,26 +22,25 @@ def grab_image_by_decimal(sct, monitor_number, top_dec, left_dec, width_dec=0.0,
         height_px = int(monitor["height"] * height_dec)
     else:
         height_px = 1
-    return sct.grab({"top": top_px,
-                     "left": left_px,
-                     "width": width_px,
-                     "height": height_px,
-                     "mon": monitor_number})
+    return {"top": top_px,
+            "left": left_px,
+            "width": width_px,
+            "height": height_px,
+            "mon": monitor_number}
 
 
 def read_elements(element_queue):
     with mss.mss() as sct:
-        for _ in range(1000):
-            sct_img = grab_image_by_decimal(sct, 1, 0.46, 0.877, 0.0, 0.0)
-
-            mss.tools.to_png(sct_img.rgb, sct_img.size, output="output.png")
+        monitor_dimensions = dec_to_px(sct, 1, 0.46, 0.493, 0.0, 0.0)
+        while True:
+            sct_img = sct.grab(monitor_dimensions)
             h, s, v = colorsys.rgb_to_hsv(*sct_img.pixels[0][0])
-            # if 0.89 < h < 0.91 and 0.98 < s and 0.42 < v < 0.43:
-                # print("NOTE FOUND")
-            # elif 0.86 < h < 0.87 and s < 0.09 and 0.98 < v:
-                # print("SUSTAIN FOUND")
+            if 0.89 < h < 0.91 and 0.98 < s and 0.42 < v < 0.43:
+                print("NOTE FOUND")
+            elif 0.86 < h < 0.87 and s < 0.09 and 0.98 < v:
+                print("SUSTAIN FOUND")
             # else:
-                # print("----------------")
+            #     print("----------------")
 
-for _ in range(10000):
-    read_elements(None)
+
+read_elements(None)
