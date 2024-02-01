9551698dddd7b93f9dfdb961af2795dbc97b9259
Andon Li <andonli281@gmail.com>
2024-02-01 13:12:46 -0500

    New Commit

--- ElementReader.py
+++ ElementReader.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import timeit
 from multiprocessing import Process, Queue
 from time import time, sleep
@@ -29,18 +30,52 @@ def dec_to_px(sct, monitor_number, top_dec, left_dec, width_dec=0.0, height_dec=
             "mon": monitor_number}
 
 
-def read_elements(element_queue):
-    with mss.mss() as sct:
-        monitor_dimensions = dec_to_px(sct, 1, 0.46, 0.493, 0.0, 0.0)
+def read_elements(element_queue: Queue):
+    with (mss.mss() as sct):
+        monitor_dimensions = dec_to_px(sct, 1, 0.447, 0.493, 0.0, 0.15)
         while True:
             sct_img = sct.grab(monitor_dimensions)
-            h, s, v = colorsys.rgb_to_hsv(*sct_img.pixels[0][0])
-            if 0.89 < h < 0.91 and 0.98 < s and 0.42 < v < 0.43:
-                print("NOTE FOUND")
-            elif 0.86 < h < 0.87 and s < 0.09 and 0.98 < v:
-                print("SUSTAIN FOUND")
-            # else:
-            #     print("----------------")
+
+            cv.imshow("window", np.array(sct_img))
+
+            release_note_counter = 0
+            note_found = False
+
+            for row in sct_img.pixels:
+
+                # row is a tuple, containing pixel elements
+                pixel_hsv = colorsys.rgb_to_hsv(*row[0])
+
+                # Purple Note
+                if 0.9 < pixel_hsv[0] < 0.92 and \
+                        0.95 < pixel_hsv[1] and \
+                        104 < pixel_hsv[2] < 111:
+                    note_found = True
+
+                # Orange Note ***not done****
+                elif 0.9 < pixel_hsv[0] < 0.92 and \
+                        0.95 < pixel_hsv[1] and \
+                        104 < pixel_hsv[2] < 111:
+                    note_found = True
+
+                # Release Note
+                elif 0.82 < pixel_hsv[0] < 0.87 and \
+                        0.05 < pixel_hsv[1] < 0.22 and \
+                        210 < pixel_hsv[2]:
+                    if release_note_counter == 10:
+                        print("release note")
+                        element_queue.put(())
+                    else:
+                        release_note_counter += 1
+                else:
+                    release_note_counter = 0
+                    if note_found:
+                        print("NOTE")
+                        element_queue.put((0, 2, ))
+                        note_found = False
+
+            cv.waitKey()
+            quit()
 
 
 read_elements(None)
