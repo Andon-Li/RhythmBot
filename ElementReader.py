0ba6ce84bf6daeae3e28fe17e114bfb7ae931d89
Andon Li <andonli281@gmail.com>
2024-02-05 18:32:12 -0500

    New Commit

--- ElementReader.py
+++ ElementReader.py
@@ -1,14 +1,7 @@
-import multiprocessing
-import timeit
-from multiprocessing import Process, Queue
-from time import time, sleep
-import win32gui
-import pygetwindow as pgw
+from multiprocessing import Queue
+from time import sleep
 import mss.tools
-import PIL.Image
-import colorsys
-import cv2 as cv
-import numpy as np
+from colorsys import rgb_to_hsv
 
 
 def dec_to_px(sct, monitor_number, top_dec, left_dec, width_dec, height_dec) -> dict:
@@ -31,18 +24,21 @@ def dec_to_px(sct, monitor_number, top_dec, left_dec, width_dec, height_dec) -> 
 
 
 def is_highway_active(sct, left_dimensions, right_dimensions) -> bool:
-    pixels = sct.grab(left_dimensions)[0] + sct.grab(right_dimensions)[0]
-    for pixel in pixels:
-        h, s, v = colorsys.rgb_to_hsv(*pixel)
-        if (h > 0.73 or h < 0.78) and \
-                (s > 0.9) and \
-                (v > 0.22 or v < 0.26):
-            return False
-
+    rows = sct.grab(left_dimensions).pixels + sct.grab(right_dimensions).pixels
+    for row in rows:
+        h, s, v = rgb_to_hsv(*row[0])
+        if h > 0.73 or h < 0.78 or \
+                s > 0.9 or \
+                v > 0.22 or v < 0.26:
+            return True
     return True
 
 
 def new_read_func(element_queue: Queue):
+    next_element_type = None
+    next_element_height = None
+    lift_element_counter = None
+
     with mss.mss() as sct:
         lane_2_px_dimensions = dec_to_px(sct, 1, 0.447, 0.493, 0.0, 0.05)
 
@@ -51,6 +47,37 @@ def new_read_func(element_queue: Queue):
 
         while not is_highway_active(sct, highway_activity_left_dimensions, highway_activity_right_dimensions):
             sleep(0.1)
+
+        lane_2_capture = sct.grab(lane_2_px_dimensions)
+
+        for idx, row in enumerate(reversed(lane_2_capture.pixels)):
+            h, s, v = rgb_to_hsv(*row[0])
+
+            # Purple Note
+            if 0.9 < h < 0.92 and \
+                    0.95 < s and \
+                    104 < v < 111:
+                next_element_type = 0
+                next_element_height = idx
+                break
+
+            # Orange Note
+            elif 0.045 < h < 0.055 and \
+                    0.97 < s and \
+                    224 < v < 235:
+                next_element_type = 1
+                next_element_height = idx
+                break
+
+            # Lift Note
+            elif 0.82 < h < 0.87 and \
+                    0.05 < s < 0.22 and \
+                    210 < v:
+                if lift_element_counter == 10:
+                    next_element_type = 2
+                    next_element_height = idx
+                else:
+                    lift_element_counter += 1
 
 
 
@@ -64,7 +91,7 @@ def read_elements(element_queue: Queue):
             note_found = False
 
             for idx, row in enumerate(sct_img.pixels):
-                h, s, v = colorsys.rgb_to_hsv(*row[0])
+                h, s, v = rgb_to_hsv(*row[0])
 
                 # Purple Note
                 if 0.9 < h < 0.92 and \
@@ -94,3 +121,5 @@ def read_elements(element_queue: Queue):
                         element_queue.put((0, 2, idx/monitor_dimensions["height"]))
                         note_found = False
 
+
+new_read_func(None)
