f6deb4d9a2daef927223f5669fd54fb798c64f1b
Andon Li <andonli281@gmail.com>
2024-02-28 13:55:06 -0500

    Polish end of game loop

--- ElementReader.py
+++ ElementReader.py
@@ -1,98 +1,10 @@
-from time import sleep, perf_counter
+from time import perf_counter
 import mss.tools
 from colorsys import rgb_to_hsv
 import cv2 as cv
 import numpy as np
 import pyautogui as pag
 import dxcam
-
-
-class AppState:
-    def __init__(self):
-        with mss.mss() as sct:
-            monitor = sct.monitors[1]
-            self.monitor_height = monitor['height']
-            self.monitor_width = monitor['width']
-        self.cap_left = int(self.monitor_width * 0.375)
-        self.cap_top = int(self.monitor_height * 0.4463)
-        self.cap_height = int(self.monitor_height * 0.1)
-        self.cap_width = int(self.monitor_width * 0.255)
-        self.cap_right = self.cap_left + self.cap_width
-        self.cap_bottom = self.cap_top + self.cap_height
-
-        self.highway_edge_idx = 0.0961, 0.0082, 0.8998, 0.9898
-
-        self.lane_0_idx = None
-        self.lane_1_idx = None
-        self.lane_2_idx = None
-        self.lane_3_idx = None
-        self.lane_4_idx = None
-
-        self.element_height_in_previous_frame = 500  # need to make dynamic
-        self.next_element_type = 0
-        self.lift_element_counter = 0
-        self.offset = 0.52
-
-    def get_cap_region(self):
-        return self.cap_left, self.cap_top, self.cap_right, self.cap_bottom
-
-
-# Bresenham's Line Algorithm
-def compute_line_idx(cap_width, cap_height, dec_x0, dec_x1):
-    x0 = int(dec_x0 * (cap_width-1))
-    y0 = 0
-    x1 = int(dec_x1 * (cap_width-1))
-    y1 = cap_height-1
-
-    indices = []
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = 1 if x0 < x1 else -1
-    sy = 1 if y0 < y1 else -1
-    err = dx - dy
-
-    while x0 != x1 or y0 != y1:
-        indices.append((x0, y0))
-
-        e2 = 2 * err
-        if e2 > -dy:
-            err -= dy
-            x0 += sx
-        if e2 < dx:
-            err += dx
-            y0 += sy
-
-    indices.append((x1, y1))
-    return indices
-
-
-def dec_to_px(sct, monitor_number, top_dec, left_dec, width_dec, height_dec) -> dict:
-    monitor = sct.monitors[monitor_number]
-    top_px = int(monitor["height"]*top_dec + monitor["top"])
-    left_px = int(monitor["width"]*left_dec + monitor["left"])
-    if width_dec:
-        width_px = int(monitor["width"] * width_dec)
-    else:
-        width_px = 1
-    if height_dec:
-        height_px = int(monitor["height"] * height_dec)
-    else:
-        height_px = 1
-    return {"top": top_px,
-            "left": left_px,
-            "width": width_px,
-            "height": height_px,
-            "mon": monitor_number}
-
-
-def is_highway_inactive(image, indices) -> bool:
-    for x, y in indices:
-        h, s, v = rgb_to_hsv(*image[y, x])
-        if h < 0.7 or h > 0.81 or \
-                s < 0.67 or \
-                v < 114 or v > 190:
-            return True
-    return False
 
 
 def initialization():
@@ -133,19 +45,67 @@ def initialization():
 
     activity = bresenham_line(0, cap_width*0.0894, cap_height)
 
-    lane_1 = bresenham_line(cap_width * 0.0437, cap_width * 0.1206, cap_height)
-    lane_2 = bresenham_line(cap_width * 0.2537, cap_width * 0.2932, cap_height)
-    lane_3 = bresenham_line(cap_width * 0.4533, cap_width * 0.4574, cap_height)
-    lane_4 = bresenham_line(cap_width * 0.6445, cap_width * 0.6134, cap_height)
-    lane_5 = bresenham_line(cap_width * 0.8379, cap_width * 0.7714, cap_height)
+    lane_0 = bresenham_line(cap_width * 0.0583, cap_width * 0.1352, cap_height)
+    lane_1 = bresenham_line(cap_width * 0.2578, cap_width * 0.2973, cap_height)
+    lane_2 = bresenham_line(cap_width * 0.4574, cap_width * 0.4616, cap_height)
+    lane_3 = bresenham_line(cap_width * 0.6549, cap_width * 0.6217, cap_height)
+    lane_4 = bresenham_line(cap_width * 0.8545, cap_width * 0.7880, cap_height)
 
     return ((cap_left, cap_top, cap_right, cap_bottom),
-            (lane_1, lane_2, lane_3, lane_4, lane_5),
+            (lane_0, lane_1, lane_2, lane_3, lane_4),
             activity)
 
 
-# game_element format: (element type(0-2), lane number(0-4), 'perf_counter' time of action)
+def is_highway_inactive(image, indices) -> bool:
+    for x, y in indices:
+        h, s, v = rgb_to_hsv(*image[y, x])
+        if h < 0.7 or h > 0.81 or \
+                s < 0.67 or \
+                v < 114 or v > 190:
+            return True
+    return False
+
+
+def matches_purple_note(h, s, v):
+    if 0.84 < h < 0.855 and \
+            0.94 < s and \
+            220 < v < 245:
+        return True
+    return False
+
+
+def matches_orange_note(h, s, v):
+    if 0.1 < h < 0.12 and \
+            0.9 < s and \
+            240 < v:
+        return True
+    return False
+
+
+def matches_purple_lift_note(h, s, v):
+    if 0.82 < h < 0.87 and \
+            0.03 < s < 0.22 and \
+            200 < v:
+        return True
+    return False
+
+
+def matches_orange_lift_note(h, s, v):
+    if (h < 0.03 or h > 0.97) and \
+            0.06 < s < 0.2 and \
+            170 < v:
+        return True
+    return False
+
+
 def read_elements(element_queue):
+    current_note_type = 0
+    current_note_height = 99999
+
+    offset = 0.5
+
+    lift_note_counter = 0
+
     cap_region, lane_indices, activity_indices = initialization()
 
     camera = dxcam.create(region=cap_region)
@@ -156,12 +116,49 @@ def read_elements(element_queue):
         if is_highway_inactive(image, activity_indices):
             continue
 
-        for lane_idx in lane_indices:
-            for x, y in lane_idx:
+        for lane_num, indexes in enumerate(lane_indices):
+            for x, y in indexes:
                 h, s, v = rgb_to_hsv(*image[y, x])
-                
 
+                if matches_purple_note(h, s, v):
+                    if y > current_note_height:
+                        element_queue.put((current_note_type, lane_num, perf_counter()+offset))
+                        current_note_type = 1
+                    current_note_height = y
+                    break
 
+                elif matches_orange_note(h, s, v):
+                    if y > current_note_height:
+                        element_queue.put((current_note_type, lane_num, perf_counter()+offset))
+                        current_note_type = 2
+                    current_note_height = y
+                    break
+
+                elif matches_orange_lift_note(h, s, v):
+                    if lift_note_counter == 9:
+                        if y > current_note_height:
+                            element_queue.put((current_note_type, lane_num, perf_counter()+offset))
+                            current_note_type = 3
+                        current_note_height = y
+                    else:
+                        lift_note_counter += 1
+                    break
+
+                elif matches_purple_lift_note(h, s, v):
+                    if lift_note_counter == 9:
+                        if y > current_note_height:
+                            element_queue.put((current_note_type, lane_num, perf_counter()+offset))
+                            current_note_type = 4
+                        current_note_height = y
+                    else:
+                        lift_note_counter += 1
+                    break
+
+                else:
+                    lift_note_counter = 0
+            else:
+                if current_note_type:
+                    element_queue.put((current_note_type, lane_num, perf_counter()+offset))
 
 
     # with mss.mss() as sct:
