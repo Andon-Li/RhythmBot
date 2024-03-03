bae0c600eda34ec4a5f530d609ed7444d3b2d942
Andon Li <andonli281@gmail.com>
2024-03-03 00:26:52 -0500

    .

--- ElementReader2.py
+++ ElementReader2.py
@@ -1,29 +1,21 @@
 from colorsys import rgb_to_hsv
 from time import perf_counter, time_ns
 import dxcam
-import mss.tools
 import csv
 import os
-
-def dec2px(*lanes):
-    with mss.mss() as sct:
-        monitor = sct.monitors[1]
-        monitor_width = monitor['width']
-    return
-
 
 def note_thinker(image, pixel_locs: tuple[int, int, int]) -> str:
     def color_match(rgb_val: tuple[int, int, int]) -> int:
         h, s, v = rgb_to_hsv(*rgb_val)
 
-        if s < .18 and 250 < v:
+        if s < .12 and 250 < v:
             return 1  # white
-        if:
+        if 0.846<h<0.863 and 0.97<s and 110<v<135:
             return 2  # purple
-        if:
+        if 0.04<h<0.07 and 0.92<s and 215<v:
             return 3  # orange
-        if:
-            return 0  # no match
+        if <h< and <s< and <v<:
+        return 0  # no match
 
     match color_match(image[pixel_locs[0]]):
         case 1:
@@ -46,15 +38,17 @@ def main():
                   (140, 169, 162),
                   (234, 265, 257),
                   (327 , 304, 308),
-                  (420, 451, 404))
+                  (420, 451, 399))
 
     camera = dxcam.create(region=(728, 579, 1201, 580))
     camera.start()
 
-    reading = True
-    while reading:
+    while True:
         image = camera.get_latest_frame()
         shot_time = perf_counter()
+
+        if len(song_map) > 500:
+            break
 
         for lane_num, lane_poi in enumerate(lanes_pois):
             note_type = note_thinker(image, lane_poi)
@@ -64,10 +58,8 @@ def main():
         os.makedirs('song_maps')
 
     with open(f'song_maps\\{time_ns()}', 'w') as file:
-        csv_writer = csv.writer()
-
-    with mss.mss() as sct:
-        sct.grab()
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(song_map)
 
 
 main()
